e8f1ab2eed34a3229d9d60def61f30728df6f18d
Jessica Downey <jessica.downey@avg.com>
2016-03-16 13:25:11 +0100

    added functionality of pw generator + reveal / hide pw if typed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,62 @@
-angular.module('app.controllers', [])
+ angular.module('app.controllers', [])
   
 .controller('createANewWiFiNetworkCtrl', function($scope) {
 
+
+})
+
+
+.controller('generatePassword', function($scope) {
+
+  // Set the default value of inputType
+  $scope.inputType = 'password';
+
+  $scope.password = '';
+
+$scope.passwords = ["ClementineYeast3910","FunkyCups9421","BlueFunnel9761"];
+
+
+$scope.generate = function() {
+
+  $scope.inputType = 'text';
+
+  $scope.password = $scope.passwords[Math.floor(Math.random() * $scope.passwords.length)];
+
+
+
+
+
+  };
+
+$scope.showpw = function() {
+
+	if ($scope.inputType == 'password')
+      $scope.inputType = 'text';
+    else
+      $scope.inputType = 'password';
+
+  };
+
+
+$scope.change = function() {
+
+	if ($scope.password == null){
+		$scope.myForm.networkpw.$setPristine();
+   		$scope.myForm.networkpw.$setUntouched();
+	}
+	
+  
+  };
+
+function addOptionToArray(optionText) {
+  passwords.push({optionText});
+} 
+
+
 })
    
+
+
 .controller('homeWifi9792CreatedCtrl', function($scope) {
 
 })
